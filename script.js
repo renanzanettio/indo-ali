a4bb7e22723f74c16890c288e59221761a397e01
Renan Zanetti <93152181+renanzanettio@users.noreply.github.com>
2023-09-30 23:56:00 -0300

    v1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", () => {
+    document.querySelector(".initial-display").classList.add("hidden");
+    document.getElementById("audio").play();
+})
